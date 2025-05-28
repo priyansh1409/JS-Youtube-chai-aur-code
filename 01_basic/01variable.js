b3c1903f22a50ff19e9e1724e25f846b1d00e726
priyansh1409 <165508010+priyansh1409@users.noreply.github.com>
2025-05-28 19:31:47 +0000

    datatypes

---
+++ 01_basic/01variable.js
@@ -0,0 +1,11 @@
+const accountId=123456
+let accountemail="priyansh2005r@gmail.com"
+var accountpassword="23232"
+accountcity="jaipur"
+let accountstate
+
+accountemail="gproiyansh@gmail.com"
+accountpassword="34345"
+accountcity="delhi"
+
+console.table([accountId,accountemail,accountpassword,accountcity,accountstate])
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,20 @@
+"use strict" //treat all js as new version
+//alrart(3+3) we are using nodejs,not brouser
+
+//datatype
+//number 2 to power 53
+//bigint
+//string->""
+//boolean
+//null standerd value
+//undefined
+//symbol=>unique
+
+//object
+
+let name="priyansh"
+let age=18
+ 
+// null ka type object
+//undefined ka type undefined
+console.log(typeof age)
